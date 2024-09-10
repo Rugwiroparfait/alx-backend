ce59f5088ca6f3d03d1861babc71fe62f4ac1139
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-09-10 20:03:26 +0200

    0-app.py pycodestyle correction

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" 
+"""
 Basic Flask app setup.
 """
 
@@ -7,12 +7,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    """ 
+    """
     Route for the home page.
     """
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
