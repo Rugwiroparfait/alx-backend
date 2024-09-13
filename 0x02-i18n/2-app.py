0b9b88e5622c8abfcf0f9dfed776ce2a3ca60b31
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-09-13 23:45:06 +0200

    2-app.py corrected

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,45 +1,40 @@
 #!/usr/bin/env python3
-'''Task 2: Get locale from request
-'''
+"""
+This is a simple Flask web application that demonstrates
+the use of Flask-Babel for internationalization (i18n).
+It renders an HTML template in the user's preferred language
+based on the `Accept-Language` header of their request.
+"""
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-class Config:
-    '''Config class'''
-
-    DEBUG = True
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
 app = Flask(__name__)
+
+
+class Config:
+    """Configuration class for setting application languages and timezone."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """Retrieves the locale for a web page.
-
-    Returns:
-        str: best match
-    """
+def get_locale():
+    """Return the best match language from the user's request."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index() -> str:
-    '''default route
-
-    Returns:
-        html: homepage
-    '''
-    return render_template("2-index.html")
+def index():
+    """Render the index template."""
+    return render_template('2-index.html')
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
